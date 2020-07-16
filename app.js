56609e68d4ac0226b8709295affb22974db9af47
Victor502 <victorahorton@gmail.com>
2020-07-15 20:13:19 -0400

    fix for android local host

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const db = require('monk')(config.storage.uri);
 // const dbName = 'cayden';
 
 
-d.listen(3000, function (err, address) {
+d.listen(3000, '0.0.0.0', function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
